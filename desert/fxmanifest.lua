c15a81755fb0751593b8422ab24d77ba57b62298
odisis <giovanni.santana033@gmail.com>
2026-03-01 15:21:44 -0300

    add4

---
+++ desert/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+author 'CpxVehiclesDevTeam'
+description 'CAR'
+version '1.0.0s'
+
+escrow_ignore {
+    'stream/*.ytd',
+    'data/*.meta',
+}
+
+files {
+    "data/handling.meta",
+    "data/vehicles.meta",
+    "data/carcols.meta",
+    "data/carvariations.meta"
+}
+
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+
+dependency '/assetpacks'
+dependency '/assetpacks'
